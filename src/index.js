be8e40a2562984766497c1c94109109c7b540219
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-06-08 23:35:47 -0300

    feat: creating library

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const uniqueRandomArray = require('unique-random-array');
+const finalSpaceCharacters = require('./final-space-characters.json');
+
+const all = () => finalSpaceCharacters;
+const random = uniqueRandomArray(finalSpaceCharacters);
+
+module.exports = {
+  all,
+  random,
+}
